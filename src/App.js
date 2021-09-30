2518981d817244fd5f6253a65689d4a1b40198bb
Joy Lee <purple402@naver.com>
2021-09-30 18:05:33 +0900

    Install create-react-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>
+            <input type="checkbox" id="2021 날짜형"></input>
+            <label for="2021 날짜형">2021 날짜형</label>
+        </li>
+        <li>
+            <input type="checkbox" id="만년형"></input>
+            <label for="만년형">만년형</label>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
