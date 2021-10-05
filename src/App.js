ee4050123fcb2ac135a14b4e0a62cad3bf77dd4e
Joy Lee <purple402@naver.com>
2021-10-05 15:57:58 +0900

    Add data of category

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 
 function App() {
+  const category = [
+    {title:"구성", sub:['연간', '월간', '주간', '일간']},
+    {title:"기간", sub:['1년+', '1년', '6개월', '3개월', '1개월']}
+  ];
   return (
     <div className="App">
       <ul>
