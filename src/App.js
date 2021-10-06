3dcc754079d7ee01240eeba46292c039a400afae
Joy Lee <purple402@naver.com>
2021-10-06 17:58:23 +0900

    Update Category Component

--- src/App.js
+++ src/App.js
@@ -4,25 +4,17 @@ import './App.css';
 
 function App() {
   const category = [
-    {title:"구성", sub:['연간', '월간', '주간', '일간']},
-    {title:"기간", sub:['1년+', '1년', '6개월', '3개월', '1개월']},
-    {title:"크기", sub:["A6-", "B6", "A5", "B5", "A4+"]},
-    {title:"커버", sub:["하드커버", "소프트커버", "PVC"]},
-    {title:"제본", sub:["양장", "6공", "스프링", "메모지"]},
-    {title:"기타", sub:["타임라인", "체크리스트", "밴드", "가름끈", "모눈", "월간-주간-월간-주간", "월간전체-주간전체"]},
+    {id: 1, title:"구성", sub:['연간', '월간', '주간', '일간']},
+    {id: 2, title:"기간", sub:['1년+', '1년', '6개월', '3개월', '1개월']},
+    {id: 3, title:"크기", sub:["A6-", "B6", "A5", "B5", "A4+"]},
+    {id: 4, title:"커버", sub:["하드커버", "소프트커버", "PVC"]},
+    {id: 5, title:"제본", sub:["양장", "6공", "스프링", "메모지"]},
+    {id: 6, title:"기타", sub:["타임라인", "체크리스트", "밴드", "가름끈", "모눈", "월간-주간-월간-주간", "월간전체-주간전체"]},
   ];
-  // 카테고리 선택을 위한 리스트 만들기
-  const lists = [];
-  let i = 0;
-  while(i < category.length){
-    lists.push(
-      <Category title={category[i].title} data={category[i].sub}></Category>
-    );
-    i = i + 1;
-  }
   return (
     <div className="App">
-      {lists}
+      {/* <h1>다이어리 키워드로 검색하기</h1> */}
+      <Category data={category}></Category>
     </div>
   );
 }
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,24 +1,43 @@
 import React from 'react';
+import './Category.css';
 
 function Category(props) {
+  // 클릭 이벤트 처리 함수
+  function handleClick(e) {
+    console.log(e.target.id);
+    console.log(e.target.checked);
+  }
+  // 리스트 구성하기
   const lists = [];
   let i = 0;
   while(i < props.data.length){
+    const catData = props.data[i];
+    // 서브 리스트 구성하기
+    let j = 0;
+    let subLists = [];
+    while(j < catData.sub.length){
+      subLists.push(
+          <li key={i * 100 + j}>
+            <input type="checkbox" id={catData.sub[j]} onClick={handleClick}>
+            </input>
+            <label htmlFor={catData.sub[j]}>{catData.sub[j]}</label>
+          </li>
+      )
+      j = j + 1;
+    }
     lists.push(
-      <li key={i}>
-        <input type="checkbox" id={props.data[i]}>
-        </input>
-        <label for={props.data[i]}>{props.data[i]}</label>
-      </li>
+      <div key={i * 100} className="mainCategory">
+        <h2 className="groupHeader">{catData.title}</h2>
+        <ul className="subCategory">
+          {subLists}
+        </ul>
+      </div>
     );
     i = i + 1;
   }
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <ul>
-        {lists}
-      </ul>
+    <div className="category">
+      {lists}
     </div>
   );
 }
