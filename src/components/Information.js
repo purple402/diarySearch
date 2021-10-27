2d00d60b5eb76b65fbcd4cc2d0117a434a909694
Joy Lee <purple402@naver.com>
2021-10-27 16:33:27 +0900

    Create Information.js for json data

---
+++ src/components/Information.js
@@ -0,0 +1,231 @@
+import React from "react";
+
+const Information = [
+    {
+        id : 1,
+    name : "워크로그 하프 다이어리(만년형, 데일리)",
+    brand : "SAILORS",
+    size : "155*216",
+    image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/416/B004166759.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+    link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4166759&amp;disp=101102&amp;pCtr=101102",
+    tag : ['만년', '연간', '월간', '일간', '프리노트', '6개월', 'A5', '하드커버', '양장', '100g', '체크리스트', '월요일시작', ]
+},
+{
+    id : 2,
+name : "앙리 마티스 양장 다이어리 (만년형)",
+brand : "MONOLIKE",
+size : "135*191",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/416/B004166581.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4166581&amp;disp=101102&amp;pCtr=101102",
+tag : ['만년', '연간', '월간', '주간', '프리노트', '월간전체-주간전체', '넓은가로형', '1년', 'A5', '하드커버', '양장', '모눈', '가름끈', ]
+},
+{
+    id : 3,
+name : "체리쉬미 다이어리",
+brand : "WANNATHIS",
+size : "120*170",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004159944.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4159944&amp;disp=101102&amp;pCtr=101102",
+tag : ['만년', '월간', '프리노트', '1년이상', 'B6', '소프트커버', '양장', '120g', '모눈', ]
+},
+{
+    id : 4,
+name : "(날짜형) 2022 클래시 젠틀 다이어리 - A5 6공",
+brand : "PLEPIC",
+size : "240*195",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004154557-4.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4154557&amp;disp=101102&amp;pCtr=101102",
+tag :['2022년', '달력', '연간', '월간', '주간', '월간-주간-월간-주간', '좁은세로형', '여유공간있음', '1년', 'A5', '가죽커버', '6공', '모눈', '포켓', ] 
+},
+{
+    id : 5,
+name : "2022 고요다이어리",
+brand : "EEDEN DESIGN",
+size : "148*210",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004154538.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4154538&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '달력', '연간', '월간', '주간', '프리노트', '월간-주간-월간-주간', '넓은세로형', '1년', 'A5', '하드커버', '양장', '가름끈', ]
+},
+{
+    id : 6,
+name : "2022 라이프앤피시스 다이어리 날짜형 - 먼슬리 L",
+brand : "LIVEWORK",
+size : "215*297",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004154371.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4154371&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '연간', '월간', '프리노트', '1년이상', 'A4이상', '소프트커버', 'PVC커버', '양장', '100g', '모눈', ]
+},
+{
+    id : 7,
+name : "2022 라이프앤피시스 다이어리 날짜형 - 먼슬리 M",
+brand : "LIVEWORK",
+size : "148*210",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004154370.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4154370&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '연간', '월간', '프리노트', '1년이상', 'A5', '소프트커버', 'PVC커버', '양장', '100g', '모눈', ]
+},
+{
+    id : 8,
+name : "2022 라이프앤피시스 다이어리 날짜형 - 먼슬리 S",
+brand : "LIVEWORK",
+size : "95*150",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004154369.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4154369&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '연간', '월간', '프리노트', '1년이상', 'A6이하', '소프트커버', 'PVC커버', '양장', '100g', '모눈', ]
+},
+{
+    id : 9,
+name : "2022 라이프앤피시스 다이어리 날짜형 - 위클리",
+brand : "LIVEWORK",
+size : "95*150",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004154368.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4154368&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '월간', '주간', '월간전체-주간전체', '좁은가로형', '여유공간있음', '1년', 'A6이하', '소프트커버', 'PVC커버', '양장', '100g', ]
+},
+{
+    id : 10,
+name : "2022 라이프앤피시스 다이어리 날짜형 - 데일리",
+brand : "LIVEWORK",
+size : "95*150",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004154367.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4154367&amp;disp=101102&amp;pCtr=101102",
+tag :  ['2022년', '월간', '일간', '1년', 'A6이하', '소프트커버', 'PVC커버', '양장', '100g', '가름끈', ]
+},
+{
+    id : 11,
+name : "데코포켓 노트 다이어리",
+brand : "BE ON D",
+size : "150*150",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004140507-1.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4140507&amp;disp=101102&amp;pCtr=101102",
+tag : ['만년', '연간', '월간', '주간', '월간-주간-월간-주간', '넓은세로형', '1년', 'A5', '소프트커버', 'PVC커버', '양장', '120g', '정사각형', ] 
+},
+{
+    id : 12,
+name : "오첵 O-CHECK 위클리 6공 만년 다이어리",
+brand : "O-CHECK",
+size : "148*210",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004149913-1.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4149913&amp;disp=101102&amp;pCtr=101102",
+tag : ['만년', '연간', '월간', '주간', '프리노트', '월간-주간-월간-주간', '넓은가로형', '1년', 'A5', '하드커버', '6공', ]
+},
+{
+    id : 13,
+name : "오첵 2022 해피데이 먼슬리플래너(B6)",
+brand : "O-CHECK",
+size : "130*190",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004149912.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4149912&amp;disp=101102&amp;pCtr=101102",
+tag :  ['2022년', '연간', '월간', '프리노트', '1년이상', 'B6', '소프트커버', 'PVC커버', '120g', ]
+},
+{
+    id : 14,
+name : "2022 레보쥬르 포켓 위클리다이어리",
+brand : "O-CHECK",
+size : "140*195",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004149903.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4149903&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '월간', '주간', '월간-주간-월간-주간', '넓은세로형', '1년이상', 'A5', '소프트커버', 'PVC커버', '양장', '100g', '포켓', ]
+},
+{
+    id : 15,
+name : "오첵 O-CHECK 2022 A4 먼슬리플래너",
+brand : "O-CHECK",
+size : "210*297",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004149874.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4149874&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '연간', '월간', '1년이상', 'A4이상', '소프트커버', 'PVC커버', '양장', '120g', ]
+},
+{
+    id : 16,
+name : "오첵 2022 B6 마이유니버스 위클리 다이어리",
+brand : "O-CHECK",
+size : "130*190",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004149873.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4149873&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '달력', '연간', '월간', '주간', '월간-주간-월간-주간', '좁은세로형', '1년', 'B6', '가죽커버', '양장', '100g', '가름끈', ]
+},
+{
+    id : 17,
+name : "오첵 O-CHECK 2022 몬저널 위클리 다이어리",
+brand : "O-CHECK",
+size : "148*210",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004149872.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4149872&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '달력', '연간', '월간', '주간', '프리노트', '월간-주간-월간-주간', '넓은세로형', '1년이상', 'A5', '하드커버', '양장', '120g', '가름끈', '밴드', ]
+},
+{
+    id : 18,
+name : "2022 친환경종이 플래너",
+brand : "두성종이",
+size : "117*128",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/413/B004139452-1.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4139452&amp;disp=101102&amp;pCtr=101102",
+tag : ['만년', '주간', '좁은가로형', '1년이상', 'A6이하', '소프트커버', '스프링', ]
+},
+{
+    id : 19,
+name : "2022 젤리 베어 다이어리",
+brand : "DAILYLIKE",
+size : "135*155",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004149858.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4149858&amp;disp=101102&amp;pCtr=101102",
+tag :  ['2022년', '달력', '연간', '월간', '주간', '프리노트', '월간전체-주간전체', '넓은세로형', '1년이상', 'A6이하', '소프트커버', '양장', '120g', '정사각형', ]
+},
+{
+    id : 20,
+name : "큐트베어 위클리 플래너 다이어리",
+brand : "RAINMALL",
+size : "145*100",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/415/B004157692.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4157692&amp;disp=101102&amp;pCtr=101102",
+tag : ['만년', '월간', '주간', '프리노트', '좁은세로형', 'A6이하', '소프트커버', 'PVC커버', '양장', ]
+},
+{
+    id : 21,
+name : "아이씨엘 of the day 다이어리2022-L",
+brand : "ICIEL01",
+size : "136*196",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004146429-1.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4146429&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '달력', '연간', '월간', '주간', '프리노트', '월간-주간-월간-주간', '넓은세로형', '1년이상', 'A5', '소프트커버', '양장', '120g', '가름끈', '밴드', ] 
+},
+{
+    id : 22,
+name : "아이씨엘 of the day 다이어리2022-S",
+brand : "ICIEL01",
+size : "118*155",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004146428.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4146428&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '달력', '연간', '월간', '주간', '프리노트', '월간-주간-월간-주간', '넓은세로형', '1년이상', 'A6이하', '소프트커버', '양장', '120g', '가름끈', '밴드', ] 
+},
+{
+    id : 23,
+name : "(2022 날짜형) 어투모먼츠 2022",
+brand : "GYOU",
+size : "148*210",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004140358.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4140358&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '달력', '연간', '월간', '주간', '프리노트', '월간-주간-월간-주간', '좁은세로형', '여유공간있음', '1년이상', 'A5', '하드커버', '양장', '120g', '가름끈', '밴드', '포켓', ]
+},
+{
+    id : 24,
+name : "콤마비 2022 월간 스케줄러 A5 핸디형",
+brand : "COMMA B",
+size : "210*148",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004146418.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4146418&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '연간', '월간', '프리노트', '1년이상', 'A5', '소프트커버', '스프링', '모눈', ]
+},
+{
+    id : 25,
+name : "콤마비 2022 월간 스케줄러 A4 라지형",
+brand : "COMMA B",
+size : "297*210",
+image : "http://thumbnail.10x10.co.kr/webimage/image/basic600/414/B004146417.jpg?cmd=thumb&amp;w=500&amp;h=500&amp;fit=true&amp;ws=false",
+link : "http://www.10x10.co.kr/shopping/category_prd.asp?itemid=4146417&amp;disp=101102&amp;pCtr=101102",
+tag : ['2022년', '연간', '월간', '프리노트', '1년이상', 'A4이상', '소프트커버', '스프링', '모눈', ]
+},
+];
+
+export default Information;
